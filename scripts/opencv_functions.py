76031421662b08c732fa5fabe2ec6c8e26e4f6e5
adityavaishampayan <aditya1.vaishampayan@gmail.com>
2020-04-07 23:08:12 -0400

    added opencv generated results

--- scripts/opencv_functions.py
+++ scripts/opencv_functions.py
@@ -25,3 +25,44 @@ SOFTWARE.
 # @copyright  MIT
 # @brief file for obtaining intrinsic and extrinsic parameters using opencv function
 
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((9*6,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+image_path = r'/home/aditya/Desktop/AutoCalib/dataset/Calibration_Imgs/'
+
+images = glob.glob(image_path + '*.jpg')
+i = 0
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+    # If found, add object points, image points (af ter refining them)
+    result_folder = r'/home/aditya/Desktop/AutoCalib/result/'
+    if ret == True:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners,(9,6),(-1,-1),criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
+        cv2.imshow('img', img)
+        cv2.imwrite(result_folder + str(i) + '.jpg',img)
+        cv2.waitKey(0)
+        i += 1
+
+cv2.destroyAllWindows()
